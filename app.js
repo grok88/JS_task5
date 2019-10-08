422ad563ccc6fb23a9d5ba04b13fd50956aa298b
Alex <grok88@tut.by>
2019-10-08 08:40:46 +0300

    Create Transform class

--- app.js
+++ app.js
@@ -1,3 +1,111 @@
+class Transform {
+
+	getArr(node){
+		let newArr = [];
+		node.forEach(elem => {
+				newArr.push({
+					url : elem.url,
+					name : elem.name,
+					params : elem.params,
+					date : elem.date
+				});
+		});
+		return newArr;
+	}
+
+	getName(elem){
+		return elem.name[0] + elem.name.substr(1).toLowerCase();
+	}
+
+	getDate(elem){
+		let date = elem.date;
+		let tmpDate = new Date(date),
+			year  = tmpDate.getFullYear(),
+			mounth = tmpDate.getMonth() + 1,
+			day = tmpDate.getDate(),
+			hours = tmpDate.getHours(),
+			minuts = tmpDate.getMinutes();
+		return `${year}/${mounth}/${day} ${hours}:${minuts}`;
+	}
+
+
+	editArr(arr){
+		return arr.map((elem) => {
+			return {
+				url : `http://${elem.url}`,
+				name : this.getName(elem),
+				params : `${elem.params.status}=>${elem.params.progress}`,
+				date : this.getDate(elem)
+			};
+		});
+	}
+
+	transform(){
+		let newArr = this.getArr(data);
+		let result = this.editArr(newArr);
+		return result;
+	}
+}
+
+let editArr = new Transform;
+let newData = editArr.transform();
+console.log(newData);
+/* 
+function print(node){
+    return console.log(node);
+}
+
+function getArr(node){
+    let newArr = [];
+    node.forEach(elem => {
+            newArr.push({
+                url : elem.url,
+                name : elem.name,
+                params : elem.params,
+                date : elem.date
+            });
+    });
+    return newArr;
+}
+
+function getName(elem){
+    return elem.name[0] + elem.name.substr(1).toLowerCase();
+}
+
+function getDate(elem){
+    let date = elem.date;
+    let tmpDate = new Date(date),
+        year  = tmpDate.getFullYear(),
+        mounth = tmpDate.getMonth() + 1,
+        day = tmpDate.getDate(),
+        hours = tmpDate.getHours(),
+        minuts = tmpDate.getMinutes();
+    return `${year}/${mounth}/${day} ${hours}:${minuts}`;
+}
+
+
+function editArr(arr){
+    return arr.map((elem) => {
+        return {
+            url : `http://${elem.url}`,
+            name : getName(elem),
+            params : `${elem.params.status}=>${elem.params.progress}`,
+            date : getDate(elem)
+        };
+    });
+}
+function transform(){
+    let newArr = getArr(data);
+    let result = editArr(newArr);
+    print(result);
+}
+transform(); */
+
+
+//-----------------------------------------------------
+
+
+
 
 var item = {
 	url: "http://desktopwallpapers.org.ua/mini/201507/40069.jpg",
